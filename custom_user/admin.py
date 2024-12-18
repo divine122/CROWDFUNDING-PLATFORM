c09cab08829c1557fc6ea6a23b58d4cb603b527d
divine <ogochukwudivine222@gmail.com>
2024-12-18 21:10:34 +0100

    Custom user created

---
+++ custom_user/admin.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+User = get_user_model()
+
+admin.site.register(User)
+
+# Register your models here.
+
+
+# Register your models here.
