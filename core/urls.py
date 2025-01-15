696cba11cfe69211da39345d8586b7a0abcb3210
divine <ogochukwudivine222@gmail.com>
2025-01-15 17:44:16 +0100

    made some changes in my settings and urls

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from campaign import views
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,33 +23,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
 
-
-
-
-
+# Swagger schema view configuration
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Crowdfunding API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Crowdfunding API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('', lambda request: HttpResponse("Welcome to Crowdfunding Platform"), name='home'),
     path('supersecrets/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),  
+    path('auth/', include('djoser.urls.jwt')),  
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),  
     path('campaign/', include('campaign.urls')),
     path('profiles/', include('profiles.urls')),
     path('customers/', include('customers.urls')),
@@ -58,18 +53,9 @@ urlpatterns = [
     path('wallet/', include('wallet.urls')),
     path('activitylog/', include('activitylog.urls')),
     path('comment/', include('comment.urls')),
-   
-    
-    
-   
-     
-
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-
